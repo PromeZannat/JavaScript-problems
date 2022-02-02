623b455710391bd85ad0dbe7b46b3afdfadfe129
Zannatul ferdous Prome <47224433+PromeZannat@users.noreply.github.com>
2022-02-02 13:11:45 +0600

    Add files via upload

---
+++ function-sum.js
@@ -0,0 +1,9 @@
+//তিনটা সংখ্যা এর যোগ করতে পারবে এমন একটা ফাংশন লিখো 
+
+function numbersSum(num1, num2, num3){
+    sum = num1 + num2 + num3;
+    return sum;
+}
+var givennumbers = numbersSum(5, 10, 15);
+
+console .log('Sum of three numbers: ', givennumbers);
